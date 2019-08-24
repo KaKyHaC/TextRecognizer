51232cf68d366b1644f7c5c6eb86d93e5fc2ac0d
Dima Barannik <dimas97@mail.ru>
2019-08-24 16:00:01 +0300

    ImagePicker.kt: init file by lazy

--- app/src/main/java/com/barannikdima/textrecognizer/utils/ImagePicker.kt
+++ app/src/main/java/com/barannikdima/textrecognizer/utils/ImagePicker.kt
@@ -2,30 +2,24 @@ package com.barannikdima.textrecognizer.utils
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
 import java.io.File
-
 
 
 object ImagePicker {
     private const val CAMERA_REQUEST = 1000
 
-    private val storageDir = Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_PICTURES)
-
-    private val file = File(storageDir, "image_to_recognize.jpg").apply {
-        createNewFile()
+    private val file by lazy {
+        val directory = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES)
+        File(directory, "image_to_recognize.jpg").apply { createNewFile() }
     }
-
-    private val fileUri = Uri.fromFile(file)
 
     fun startCameraActivity(activity: Activity) {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
-            putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+            putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file))
             resolveActivity(activity.packageManager)?.let {
                 activity.startActivityForResult(this, CAMERA_REQUEST)
             }
@@ -34,8 +28,6 @@ object ImagePicker {
 
     fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            val bitmap = BitmapFactory.decodeFile(file.absolutePath)
-            Log.d("ImagePicker", "bitmap = $bitmap")
         }
     }
 }
