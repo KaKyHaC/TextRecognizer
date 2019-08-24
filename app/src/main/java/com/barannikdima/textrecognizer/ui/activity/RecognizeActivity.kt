0a8d8d6b7db20faf60d3fed10d7ad31859c789ec
Dima Barannik <dimas97@mail.ru>
2019-08-24 17:55:13 +0300

    create RecognizeActivity.kt

---
+++ app/src/main/java/com/barannikdima/textrecognizer/ui/activity/RecognizeActivity.kt
@@ -0,0 +1,13 @@
+package com.barannikdima.textrecognizer.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.barannikdima.textrecognizer.R
+
+class RecognizeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recognize)
+    }
+}
