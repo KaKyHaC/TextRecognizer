6eb12c57a6aad5038469f66cbf8a371e4512e27f
Dima Barannik <dimas97@mail.ru>
2019-08-24 15:24:53 +0300

    MainActivity.kt: startCameraActivity

--- app/src/main/java/com/barannikdima/textrecognizer/MainActivity.kt
+++ app/src/main/java/com/barannikdima/textrecognizer/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.barannikdima.textrecognizer
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView(){
+        camera_btn.setOnClickListener(::startCameraActivity)
+    }
+
+    private fun startCameraActivity(view: View) {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (cameraIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(cameraIntent, CAMERA_INTENT)
+        }
+    }
+
+    companion object{
+        private const val CAMERA_INTENT = 1000
     }
 }
